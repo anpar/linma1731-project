Fix stupid weights normalization

src/plots.py
@@ -70,31 +70,20 @@ def plot_mes_vs_real(export=False, filename='q2-mes-vs-real.pdf'):
         fig.savefig(PATH + filename)
 
 def plot_samples_hist(export=False, filename='q3-samples-hist.pdf'):
-    t_tot = 5
+    t_tot = 16
     dt = 0.001
     ts = 0.01
     L = int(ts/dt)
 
     xs, ys, zs = simulate(t_tot)
-    print(len(xs))
     xs_m = measure(xs, L)
-    print(len(xs_m))
 
     x_tilde, _, _, wxs = classical_smc(xs_m, t_tot, L)
-    #print(x_tilde[int(5/ts), :])
-    #print(xs[int(5/dt)])
+    print(x_tilde[int(5/ts), :])
+    print(xs[int(5/dt)])
 
-    a = np.arange(0, int(t_tot/dt) + 1, 1)
-    x = np.array([xs[:-1:L], ys[:-1:L], zs[:-1:L]])
-    print(wxs)
-    print(np.linalg.norm(x-wxs, axis=0))
-    plt.plot(a[:-1:L], np.linalg.norm(x-wxs, axis=0), color='b')
-    plt.show()
-    """
-    result = plt.hist(np.around(x_tilde[int(5/ts), :], 4), bins=20, color='b',
-                     align='mid')
-    plt.axvline(np.mean(x_tilde[int(5/ts), :]), color='k', linestyle='dashed',
-                                linewidth=2)
+    result = plt.hist(x_tilde[int(5/ts), :], color='b', align='mid')
+    plt.axvline(np.mean(x_tilde[int(5/ts), :]), color='k', linestyle='dashed', linewidth=2)
     plt.axvline(xs[int(5/dt)], color='g', linestyle='dashed', linewidth=2)
     plt.show()
 
@@ -109,9 +98,7 @@ def plot_samples_hist(export=False, filename='q3-samples-hist.pdf'):
                                 linewidth=2)
     plt.axvline(xs[int(15/dt)], color='g', linestyle='dashed', linewidth=2)
     plt.show()
-    """
 
-    """
     a = np.arange(0, int(t_tot/dt) + 1, 1)
     fig, ax = plt.subplots()
     ax.plot(a, xs, 'b', label='First coordinate trajectory (real)')
@@ -127,7 +114,7 @@ def plot_samples_hist(export=False, filename='q3-samples-hist.pdf'):
         label.set_linewidth(1.5)
 
     plt.show()
-    """
+
 def main():
     #plot_3d_trajectory(False)
     #plot_mes_vs_real(False)

src/utils.py
@@ -43,12 +43,12 @@ def next_state_vector(x, y, z, a=10, r=28, b=8/3, dt=0.001, Gamma=np.eye(3)):
     """
 
     eps = np.finfo(np.float32).eps
-    u = np.random.normal(0, 10*eps, 3)
+    u = np.random.normal(0, 100000*eps, 3)
 
     return F(x, y, z, a, r, b, dt) + Gamma.dot(u)
 
 def simulate(t_tot, mu_0=1, sigma_0=math.sqrt(0.001), a=10, r=28, b=8/3, dt=0.001, Gamma=np.eye(3)):
-    """Simulate the Lorenz.
+    """Simulate the Lorenz system.
 
     Arguments:
     t_tot -- simulation time (in seconds)
@@ -95,6 +95,8 @@ def print_progress(perc):
     sys.stdout.flush()
 
 def next_state_vector_L(x, y, z, L, a=10, r=28, b=8/3, dt=0.001, Gamma=np.eye(3)):
+    """ Apply next_state_vector L times."""
+
     x_cur = np.copy(x)
     y_cur = np.copy(y)
     z_cur = np.copy(z)
@@ -137,13 +139,12 @@ def classical_smc(xs_m, t_tot, L, n=100, mu_0=1, sigma_0=math.sqrt(0.001), sigma
         for i in range(n):
             weights[i] = scipy.stats.norm.pdf(x_tilde[t, i], xs_m[t],
                                               sigma_m)
-            weights = abs(weights)
-            weights /= sum(weights)
+
+        weights /= sum(weights)
 
         for i in range(n):
             wxs[:, t] += weights[i] * np.array([x_tilde[t, i], y_tilde[t, i],
                                           z_tilde[t, i]])
-
 
         # Resample the particles according to the weights
         ind_sample = np.random.choice(np.arange(n), n, True, weights)